c14dc32df21b82fcf08a5cd60bef37e537395d23
Patrick Zava <patrickzava@gmail.com>
2019-06-10 16:24:37 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { fragment } from "react";
+
+import GlobalStyle from "./styles/global";
+
+import Main from "./pages/Main";
+
+const App = () => (
+  <fragment>
+    <GlobalStyle />
+    <Main />
+  </fragment>
+);
+
+export default App;
